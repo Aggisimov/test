68b5f8a9a5d784a0080d742e47c3e897e0bd58c7
Aggisimov <jacob.argeus@hotmail.com>
2025-09-02 14:22:02 +0200

    Euclidean distance

--- test.py
+++ test.py
@@ -3,8 +3,16 @@ import math
 # p1 (3,5) 
 # p2 (-2,4)
 
-x1=3
-x2=-2
-y1=5
-y2=4
+x1 = 3
+x2 = -2
+y1 = 5
+y2 = 4
 
+dx = x2 - x1
+dy = y2 - y1
+print(dx)
+print(dy)
+
+distance = math.sqrt((dx ** 2) + (dy ** 2))
+
+print(f"{distance:.1f}")
