df6b8b74a75d72fd5f03b4a724014a60be9ff15b
Aggisimov <jacob.argeus@hotmail.com>
2025-09-02 20:56:56 +0200

    3

--- if statement 01.py
+++ if statement 01.py
@@ -1,11 +1,15 @@
-#Ask the user to input two numbers and check which one is the smallest and print it out.
+# Ask the user to input three angles and check if the triangle has a right angle.
+# Your code should make sure that all three angles are valid and make up a triangle.
 
-print("hi u are going to give me 2 numbers")
+print("give me three angles of a triangle of your choosing")
 
-num1 = int(input("Give me the first number: "))
-num2 = int(input("Give me the second number"))
+a = int(input("Ange första vinkeln: "))
+b = int(input("Ange andra vinkeln: "))
+c = int(input("Ange tredje vinkeln: "))
 
-if num1 < num2:
-    print("Your second number is bigger than your first")
+if a + b + c == 180 and (a == 90 or b == 90 or c == 90):
+    print("wow u managed to do a triangle and it is a right-angled triangle")
+elif a + b + c == 180:
+    print("wow u managed to do a triangle")
 else:
-    print("Your first numver is bigger than your second")
+    print("ojojoj en triangle har totalt 180 grader")
