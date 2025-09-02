489a4ddaecb44c3518af2ba695283304a5cdc586
Aggisimov <jacob.argeus@hotmail.com>
2025-09-02 12:45:07 +0200

    Line

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import math
 
-tp = 2
-fp=2
-fn=11
-tn=985
+# A: 4,4
+# B: 0,-1
+# k= delta a / delta b
+# y= kx + m
 
-acc = (tp+tn)/(tp+fp+fn+tn)
+y1 = 4
+y2 = 1
+x1 = 4
+x2 = 0
 
-print(f"{acc*100} %" )  
+k = (y2-y1)/(x2-x1)
+print(k)
+
+m = y1-(k*x1)
+
+print(m)
