745bf09d4d71de1a00360c670df729e230aa1255
Aggisimov <jacob.argeus@hotmail.com>
2025-09-02 20:07:55 +0200

    if statements

---
+++ if statement 01.py
@@ -0,0 +1,8 @@
+number = int(input("Give me a number negetive or positive"))
+
+if number == 0:
+    print(f"your numer is {number} and its 0")
+elif number <0:
+    print(f"your number is {number} and its lover than 0")
+else:
+    print(f"your number is {number} and its higher than 0")
